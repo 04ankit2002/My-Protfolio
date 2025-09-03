f59b1c245f5663094d1a4ca2ba1a745a994cac70
Ankit Chauhan <44ankitchauhan@gmail.com>
2025-09-04 01:57:02 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      base: "/My Protfolio",
-    }
-  }
+    },
+  },
+  base: process.env.VITE_BASE_PATH || "/My-Protfolio",
 });
